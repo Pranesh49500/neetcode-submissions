7466fb7bf2cd15d6debb979c7fb1ee917d6129fe
Pranesh49500 <pranesh2370060@ssn.edu.in>
2026-07-12 17:00:36 +0530

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mySqrt(int x) {
+        int l=0;
+        int r=x;
+        int mid;
+
+        while(l<=r){
+            mid=l+(r-l)/2;
+            long sq=(long)mid*mid;
+            if(sq==x)
+                return mid;
+            else if(sq<=x)
+                l=mid+1;
+            else
+                r=mid-1;
+        }
+        return r;
+    }
+}
